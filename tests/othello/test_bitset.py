c4a051e1b449137aea10c9001d5428a8ce379b2d
Luuk Verweij <luuk_verweij@msn.com>
2024-05-04 17:11:35 +0200

    add tests for BitSet and Position

--- tests/othello/test_bitset.py
+++ tests/othello/test_bitset.py
@@ -1,1 +1,429 @@
-# TODO add tests for BitSet
+import pytest
+
+from flippy.othello.bitset import BITSET_MASK, BitSet
+
+BITSET_EMPTY = BitSet(0x0)
+
+BITSET_LEFT_TOP = BitSet(0x1)
+BITSET_RIGHT_TOP = BitSet(0x80)
+BITSET_LEFT_BOTTOM = BitSet(0x0100000000000000)
+BITSET_RIGHT_BOTTOM = BitSet(0x8000000000000000)
+
+
+BITSET_SECOND_BIT = BitSet(0x2)
+
+BITSET_TOP_EDGE = BitSet(0xFF)
+BITSET_BOTTOM_EDGE = BitSet(0xFF00000000000000)
+BITSET_LEFT_EDGE = BitSet(0x0101010101010101)
+BITSET_RIGHT_EDGE = BitSet(0x8080808080808080)
+
+BITSET_FULL = BitSet(BITSET_MASK)
+
+
+@pytest.mark.parametrize(
+    ["value"],
+    [
+        pytest.param(-1, id="too-small"),
+        pytest.param(BITSET_MASK + 1, id="too-big"),
+    ],
+)
+def test_init_error(value: int) -> None:
+    with pytest.raises(ValueError):
+        BitSet(value)
+
+
+@pytest.mark.parametrize(
+    ["rotation"],
+    [
+        pytest.param(-1, id="too-small"),
+        pytest.param(8, id="too-big"),
+    ],
+)
+def test_rotated_error(rotation: int) -> None:
+    with pytest.raises(ValueError):
+        BITSET_EMPTY.rotated(rotation)
+
+
+def test_rotated_makes_copy() -> None:
+    for rotation in range(8):
+        rotated = BITSET_EMPTY.rotated(rotation)
+        rotated is not BITSET_EMPTY
+
+
+@pytest.mark.parametrize(
+    ["bit_set"],
+    [
+        pytest.param(BITSET_EMPTY, id="empty"),
+        pytest.param(BITSET_FULL, id="full"),
+    ],
+)
+def test_rotated_unchanging(bit_set: BitSet) -> None:
+    for rotation in range(8):
+        assert bit_set.rotated(rotation) == bit_set
+
+
+def test_rotated_corner_bits() -> None:
+    expected_bit_sets = [
+        BITSET_LEFT_TOP,
+        BITSET_RIGHT_TOP,
+        BITSET_LEFT_BOTTOM,
+        BITSET_RIGHT_BOTTOM,
+        BITSET_LEFT_TOP,
+        BITSET_LEFT_BOTTOM,
+        BITSET_RIGHT_TOP,
+        BITSET_RIGHT_BOTTOM,
+    ]
+
+    for rotation in range(8):
+        rotated = BITSET_LEFT_TOP.rotated(rotation)
+        expected = expected_bit_sets[rotation]
+        assert expected == rotated
+
+
+def test_rotated_second_bit() -> None:
+    expected_bit_offsets = [1, 6, 57, 62, 8, 48, 15, 55]
+
+    for rotation in range(8):
+        rotated = BITSET_SECOND_BIT.rotated(rotation)
+        expected = BitSet(1 << expected_bit_offsets[rotation])
+        assert expected == rotated
+
+
+def test_rotated_edge() -> None:
+    expected_bit_sets = [
+        BITSET_TOP_EDGE,
+        BITSET_TOP_EDGE,
+        BITSET_BOTTOM_EDGE,
+        BITSET_BOTTOM_EDGE,
+        BITSET_LEFT_EDGE,
+        BITSET_LEFT_EDGE,
+        BITSET_RIGHT_EDGE,
+        BITSET_RIGHT_EDGE,
+    ]
+
+    for rotation in range(8):
+        rotated = BITSET_TOP_EDGE.rotated(rotation)
+        expected = expected_bit_sets[rotation]
+        assert expected == rotated
+
+
+@pytest.mark.parametrize(
+    ["bit_set"],
+    [
+        pytest.param(BITSET_EMPTY, id="empty"),
+        pytest.param(BITSET_LEFT_TOP, id="left-top"),
+        pytest.param(BITSET_SECOND_BIT, id="second-bit"),
+        pytest.param(BITSET_TOP_EDGE, id="top-edge"),
+        pytest.param(BITSET_FULL, id="full"),
+    ],
+)
+def test_unrotated(bit_set: BitSet) -> None:
+    for rotation in range(8):
+        bit_set.rotated(rotation).unrotated(rotation) == bit_set
+
+
+def test_is_set_true() -> None:
+    assert BITSET_LEFT_TOP.is_set(0)
+
+
+def test_is_set_false() -> None:
+    assert not BITSET_LEFT_TOP.is_set(1)
+
+
+@pytest.mark.parametrize(
+    ["index"],
+    [
+        pytest.param(-1, id="too-small"),
+        pytest.param(64, id="too-big"),
+    ],
+)
+def test_is_set_error(index: int) -> None:
+    with pytest.raises(ValueError):
+        assert BITSET_LEFT_TOP.is_set(index)
+
+
+def test_is_set_2d_true() -> None:
+    assert BITSET_LEFT_TOP.is_set_2d(y=0, x=0)
+
+
+def test_is_set_2d_false() -> None:
+    assert not BITSET_LEFT_TOP.is_set_2d(y=0, x=1)
+
+
+@pytest.mark.parametrize(
+    ["x", "y"],
+    [
+        pytest.param(-1, 0, id="x-too-small"),
+        pytest.param(8, 0, id="x-too-big"),
+        pytest.param(0, -1, id="y-too-small"),
+        pytest.param(0, 8, id="y-too-big"),
+        pytest.param(-1, 8, id="both-invalid"),
+    ],
+)
+def test_is_set_2d_error(x: int, y: int) -> None:
+    with pytest.raises(ValueError):
+        assert BITSET_LEFT_TOP.is_set_2d(y=y, x=x)
+
+
+@pytest.mark.parametrize(
+    ["bit_set", "expected"],
+    [
+        pytest.param(BITSET_EMPTY, True, id="empty"),
+        pytest.param(BITSET_LEFT_TOP, False, id="left-top"),
+        pytest.param(BITSET_SECOND_BIT, False, id="second-bit"),
+        pytest.param(BITSET_TOP_EDGE, False, id="top-edge"),
+        pytest.param(BITSET_FULL, False, id="full"),
+    ],
+)
+def test_is_empty(bit_set: BitSet, expected: bool) -> None:
+    assert expected == bit_set.is_empty()
+
+
+@pytest.mark.parametrize(
+    ["bit_set", "expected"],
+    [
+        pytest.param(BITSET_EMPTY, False, id="empty"),
+        pytest.param(BITSET_LEFT_TOP, True, id="left-top"),
+        pytest.param(BITSET_SECOND_BIT, True, id="second-bit"),
+        pytest.param(BITSET_TOP_EDGE, True, id="top-edge"),
+        pytest.param(BITSET_FULL, True, id="full"),
+    ],
+)
+def test_has_any(bit_set: BitSet, expected: bool) -> None:
+    assert expected == bit_set.has_any()
+
+
+@pytest.mark.parametrize(
+    ["bit_set", "expected"],
+    [
+        pytest.param(BITSET_EMPTY, 0, id="empty"),
+        pytest.param(BITSET_LEFT_TOP, 1, id="left-top"),
+        pytest.param(BITSET_SECOND_BIT, 1, id="second-bit"),
+        pytest.param(BITSET_TOP_EDGE, 8, id="top-edge"),
+        pytest.param(BITSET_FULL, 64, id="full"),
+    ],
+)
+def test_count_bits(bit_set: BitSet, expected: int) -> None:
+    assert expected == bit_set.count_bits()
+
+
+@pytest.mark.parametrize(
+    ["bit_set", "expected"],
+    [
+        pytest.param(BITSET_LEFT_TOP, 0, id="left-top"),
+        pytest.param(BITSET_SECOND_BIT, 1, id="second-bit"),
+        pytest.param(BITSET_TOP_EDGE, 0, id="top-edge"),
+        pytest.param(BITSET_FULL, 0, id="full"),
+    ],
+)
+def test_lowest_bit_index(bit_set: BitSet, expected: int) -> None:
+    assert expected == bit_set.lowest_bit_index()
+
+
+def test_lowest_bit_index_error() -> None:
+    with pytest.raises(ValueError):
+        BITSET_EMPTY.lowest_bit_index()
+
+
+def test_show_empty(capsys: pytest.CaptureFixture[str]) -> None:
+    BITSET_EMPTY.show()
+
+    printed = capsys.readouterr().out
+    expected = (
+        "+-a-b-c-d-e-f-g-h-+\n"
+        "1                 |\n"
+        "2                 |\n"
+        "3                 |\n"
+        "4                 |\n"
+        "5                 |\n"
+        "6                 |\n"
+        "7                 |\n"
+        "8                 |\n"
+        "+-----------------+\n"
+    )
+
+    assert expected == printed
+
+
+def test_show_left_top(capsys: pytest.CaptureFixture[str]) -> None:
+    BITSET_LEFT_TOP.show()
+
+    printed = capsys.readouterr().out
+    expected = (
+        "+-a-b-c-d-e-f-g-h-+\n"
+        "1 ●               |\n"
+        "2                 |\n"
+        "3                 |\n"
+        "4                 |\n"
+        "5                 |\n"
+        "6                 |\n"
+        "7                 |\n"
+        "8                 |\n"
+        "+-----------------+\n"
+    )
+
+    assert expected == printed
+
+
+def test_show_second_bit(capsys: pytest.CaptureFixture[str]) -> None:
+    BITSET_SECOND_BIT.show()
+
+    printed = capsys.readouterr().out
+    expected = (
+        "+-a-b-c-d-e-f-g-h-+\n"
+        "1   ●             |\n"
+        "2                 |\n"
+        "3                 |\n"
+        "4                 |\n"
+        "5                 |\n"
+        "6                 |\n"
+        "7                 |\n"
+        "8                 |\n"
+        "+-----------------+\n"
+    )
+
+    assert expected == printed
+
+
+def test_show_top_edge(capsys: pytest.CaptureFixture[str]) -> None:
+    BITSET_TOP_EDGE.show()
+
+    printed = capsys.readouterr().out
+    expected = (
+        "+-a-b-c-d-e-f-g-h-+\n"
+        "1 ● ● ● ● ● ● ● ● |\n"
+        "2                 |\n"
+        "3                 |\n"
+        "4                 |\n"
+        "5                 |\n"
+        "6                 |\n"
+        "7                 |\n"
+        "8                 |\n"
+        "+-----------------+\n"
+    )
+
+    assert expected == printed
+
+
+def test_show_full(capsys: pytest.CaptureFixture[str]) -> None:
+    BITSET_FULL.show()
+
+    printed = capsys.readouterr().out
+    expected = (
+        "+-a-b-c-d-e-f-g-h-+\n"
+        "1 ● ● ● ● ● ● ● ● |\n"
+        "2 ● ● ● ● ● ● ● ● |\n"
+        "3 ● ● ● ● ● ● ● ● |\n"
+        "4 ● ● ● ● ● ● ● ● |\n"
+        "5 ● ● ● ● ● ● ● ● |\n"
+        "6 ● ● ● ● ● ● ● ● |\n"
+        "7 ● ● ● ● ● ● ● ● |\n"
+        "8 ● ● ● ● ● ● ● ● |\n"
+        "+-----------------+\n"
+    )
+
+    assert expected == printed
+
+
+@pytest.mark.parametrize(
+    ["bit_set", "expected"],
+    [
+        pytest.param(BITSET_EMPTY, set(), id="empty"),
+        pytest.param(BITSET_LEFT_TOP, {0}, id="left-top"),
+        pytest.param(BITSET_SECOND_BIT, {1}, id="second-bit"),
+        pytest.param(BITSET_TOP_EDGE, set(range(8)), id="top-edge"),
+        pytest.param(BITSET_FULL, set(range(64)), id="full"),
+    ],
+)
+def test_as_set(bit_set: BitSet, expected: set[int]) -> None:
+    assert expected == bit_set.as_set()
+
+
+@pytest.mark.parametrize(
+    ["lhs", "rhs", "expected"],
+    [
+        pytest.param(BITSET_EMPTY, BITSET_EMPTY, True, id="equal"),
+        pytest.param(BITSET_EMPTY, BITSET_LEFT_TOP, False, id="not-equal"),
+    ],
+)
+def test_eq_ok(lhs: BitSet, rhs: BitSet, expected: bool) -> None:
+    assert (lhs == rhs) == expected
+
+
+def test_eq_error() -> None:
+    with pytest.raises(TypeError):
+        assert BITSET_EMPTY == 0
+
+
+@pytest.mark.parametrize(
+    ["bit_set", "expected"],
+    [
+        pytest.param(BITSET_EMPTY, BITSET_FULL, id="empty"),
+        pytest.param(BITSET_TOP_EDGE, BitSet(0xFFFFFFFFFFFFFF00), id="top-edge"),
+    ],
+)
+def test_invert(bit_set: BitSet, expected: BitSet) -> None:
+    assert ~bit_set == expected
+
+
+@pytest.mark.parametrize(
+    ["lhs", "rhs", "expected"],
+    [
+        pytest.param(
+            BITSET_LEFT_TOP, BITSET_LEFT_TOP, BITSET_LEFT_TOP, id="bitset-rhs"
+        ),
+        pytest.param(BITSET_LEFT_TOP, 0, BITSET_EMPTY, id="integer-rhs"),
+    ],
+)
+def test_and(lhs: BitSet, rhs: int | BitSet, expected: BitSet) -> None:
+    assert (lhs & rhs) == expected
+
+
+@pytest.mark.parametrize(
+    ["lhs", "rhs", "expected"],
+    [
+        pytest.param(
+            BITSET_LEFT_TOP, BITSET_LEFT_TOP, BITSET_LEFT_TOP, id="bitset-rhs"
+        ),
+        pytest.param(BITSET_LEFT_TOP, 0, BITSET_LEFT_TOP, id="integer-rhs"),
+    ],
+)
+def test_or(lhs: BitSet, rhs: int | BitSet, expected: BitSet) -> None:
+    assert (lhs | rhs) == expected
+
+
+def test_bool() -> None:
+    with pytest.raises(NotImplementedError):
+        bool(BITSET_EMPTY)
+
+
+def test_lshift_ok() -> None:
+    assert BITSET_TOP_EDGE << 4 == BitSet(0xFF0)
+
+
+@pytest.mark.parametrize(
+    ["shift"],
+    [
+        pytest.param(-1, id="too-small"),
+        pytest.param(64, id="too-big"),
+    ],
+)
+def test_lshift_error(shift: int) -> None:
+    with pytest.raises(ValueError):
+        BITSET_EMPTY << shift
+
+
+def test_rshift_ok() -> None:
+    assert BITSET_TOP_EDGE >> 4 == BitSet(0xF)
+
+
+@pytest.mark.parametrize(
+    ["shift"],
+    [
+        pytest.param(-1, id="too-small"),
+        pytest.param(64, id="too-big"),
+    ],
+)
+def test_rshift_error(shift: int) -> None:
+    with pytest.raises(ValueError):
+        BITSET_EMPTY >> shift
--- tests/othello/test_position.py
+++ tests/othello/test_position.py
@@ -1,1 +1,408 @@
-# TODO add tests for Position
+import pytest
+from typing import Iterable
+
+from flippy.othello.bitset import BitSet
+from flippy.othello.position import PASS_MOVE, InvalidMove, Position
+
+POSITION_START = Position.start()
+POSITION_EMPTY = Position.empty()
+POSITION_AFTER_ONE_MOVE = Position.start().do_move(19)
+
+POSITION_NO_MOVES_WIN = Position(BitSet(0xFFFF), BitSet(0x0))
+POSITION_NO_MOVES_DRAW = Position(BitSet(0xFFFF), BitSet(0xFFFF0000))
+POSITION_NO_MOVES_LOSS = Position(BitSet(0x0), BitSet(0xFFFF))
+
+POSITION_NEED_TO_PASS = Position(BitSet(0x2), BitSet(0x1))
+
+
+def test_init_error() -> None:
+    with pytest.raises(ValueError):
+        Position(BitSet(0x1), BitSet(0x1))
+
+
+@pytest.mark.parametrize(
+    ["position", "expected"],
+    [
+        pytest.param(
+            POSITION_START,
+            b"\x00\x00\x00\x10\x08\x00\x00\x00\x00\x00\x00\x08\x10\x00\x00\x00",
+            id="start",
+        ),
+        pytest.param(
+            POSITION_EMPTY,
+            b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+            id="empty",
+        ),
+        pytest.param(
+            POSITION_AFTER_ONE_MOVE,
+            b"\x00\x00\x00\x00\x10\x00\x00\x00\x00\x00\x08\x18\x08\x00\x00\x00",
+            id="after-one-move",
+        ),
+    ],
+)
+def test_to_bytes(position: Position, expected: bytes) -> None:
+    assert position.to_bytes() == expected
+
+
+@pytest.mark.parametrize(
+    ["position"],
+    [
+        pytest.param(POSITION_START, id="start"),
+        pytest.param(POSITION_EMPTY, id="empty"),
+        pytest.param(POSITION_AFTER_ONE_MOVE, id="after-one-move"),
+    ],
+)
+def test_from_bytes(position: Position) -> None:
+    bytes_ = position.to_bytes()
+    assert Position.from_bytes(bytes_) == position
+
+
+@pytest.mark.parametrize(
+    ["position", "expected_valid_moves"],
+    [
+        pytest.param(POSITION_START, {19, 26, 37, 44}, id="start"),
+        pytest.param(POSITION_EMPTY, {PASS_MOVE}, id="empty"),
+        pytest.param(POSITION_AFTER_ONE_MOVE, {18, 20, 34}, id="after-one-move"),
+        pytest.param(POSITION_NO_MOVES_WIN, {PASS_MOVE}, id="no-moves"),
+        pytest.param(POSITION_NEED_TO_PASS, {PASS_MOVE}, id="need-to-pass"),
+    ],
+)
+def test_is_valid_move(position: Position, expected_valid_moves: set[int]) -> None:
+    moves = list(range(64)) + [PASS_MOVE]
+
+    for move in moves:
+        expected = move in expected_valid_moves
+        assert position.is_valid_move(move) == expected
+
+
+@pytest.mark.parametrize(
+    ["position"],
+    [
+        pytest.param(POSITION_START, id="start"),
+        pytest.param(POSITION_EMPTY, id="empty"),
+        pytest.param(POSITION_AFTER_ONE_MOVE, id="after-one-move"),
+        pytest.param(POSITION_NO_MOVES_WIN, id="no-moves"),
+        pytest.param(POSITION_NEED_TO_PASS, id="need-to-pass"),
+    ],
+)
+def test_get_moves(position: Position) -> None:
+    moves = position.get_moves()
+
+    for index in range(64):
+        assert position.is_valid_move(index) == moves.is_set(index)
+
+
+def test_do_move_pass() -> None:
+    position = POSITION_NO_MOVES_LOSS
+    child = position.do_move(PASS_MOVE)
+    expected = Position(position.opp, position.me)
+    assert child == expected
+
+
+def test_do_move_square_taken() -> None:
+    with pytest.raises(InvalidMove):
+        POSITION_NO_MOVES_LOSS.do_move(0)
+
+
+def test_do_move_no_flips() -> None:
+    with pytest.raises(InvalidMove):
+        POSITION_NO_MOVES_LOSS.do_move(63)
+
+
+def test_do_move_ok() -> None:
+    child = POSITION_AFTER_ONE_MOVE.do_move(18)
+    expected = Position(BitSet(0x810080000), BitSet(0x1008040000))
+    assert child == expected
+
+
+@pytest.mark.parametrize(
+    ["position"],
+    [
+        pytest.param(POSITION_START, id="start"),
+        pytest.param(POSITION_EMPTY, id="empty"),
+        pytest.param(POSITION_AFTER_ONE_MOVE, id="after-one-move"),
+        pytest.param(POSITION_NO_MOVES_WIN, id="no-moves"),
+        pytest.param(POSITION_NEED_TO_PASS, id="need-to-pass"),
+    ],
+)
+def test_rotated(position: Position) -> None:
+    for rotation in range(8):
+        assert position.rotated(rotation).unrotated(rotation) == position
+
+
+@pytest.mark.parametrize(
+    ["position"],
+    [
+        pytest.param(POSITION_START, id="start"),
+        pytest.param(POSITION_EMPTY, id="empty"),
+        pytest.param(POSITION_AFTER_ONE_MOVE, id="after-one-move"),
+        pytest.param(POSITION_NO_MOVES_WIN, id="no-moves"),
+        pytest.param(POSITION_NEED_TO_PASS, id="need-to-pass"),
+    ],
+)
+def test_normalize(position: Position) -> None:
+    normalized, rotation = position.normalize()
+    assert normalized.unrotated(rotation) == position
+
+
+@pytest.mark.parametrize(
+    ["position"],
+    [
+        pytest.param(POSITION_START, id="start"),
+        pytest.param(POSITION_EMPTY, id="empty"),
+        pytest.param(POSITION_AFTER_ONE_MOVE, id="after-one-move"),
+        pytest.param(POSITION_NO_MOVES_WIN, id="no-moves"),
+        pytest.param(POSITION_NEED_TO_PASS, id="need-to-pass"),
+    ],
+)
+def test_normalized(position: Position) -> None:
+    normalized = position.normalized()
+
+    for rotation in range(8):
+        assert position.rotated(rotation).normalized() == normalized
+
+
+def test_pass_move() -> None:
+    position = POSITION_START
+    child = position.pass_move()
+    assert child == Position(position.opp, position.me)
+    assert child.me is not position.opp
+    assert child.opp is not position.me
+
+
+@pytest.mark.parametrize(
+    ["position", "expected"],
+    [
+        pytest.param(POSITION_START, False, id="start"),
+        pytest.param(POSITION_EMPTY, True, id="empty"),
+        pytest.param(POSITION_AFTER_ONE_MOVE, False, id="after-one-move"),
+        pytest.param(POSITION_NO_MOVES_WIN, True, id="no-moves"),
+        pytest.param(POSITION_NEED_TO_PASS, False, id="need-to-pass"),
+    ],
+)
+def test_is_game_end(position: Position, expected: bool) -> None:
+    assert position.is_game_end() == expected
+
+
+@pytest.mark.parametrize(
+    ["position", "expected"],
+    [
+        pytest.param(POSITION_START, 0, id="start"),
+        pytest.param(POSITION_EMPTY, 0, id="empty"),
+        pytest.param(POSITION_AFTER_ONE_MOVE, -62, id="after-one-move"),
+        pytest.param(POSITION_NO_MOVES_WIN, 64, id="no-moves-win"),
+        pytest.param(POSITION_NO_MOVES_DRAW, 0, id="no-moves-draw"),
+        pytest.param(POSITION_NO_MOVES_LOSS, -64, id="no-moves-loss"),
+        pytest.param(POSITION_NEED_TO_PASS, 0, id="need-to-pass"),
+    ],
+)
+def test_get_final_score(position: Position, expected: int) -> None:
+    assert position.get_final_score() == expected
+
+
+def test_show_start(capsys: pytest.CaptureFixture[str]) -> None:
+    POSITION_START.show()
+
+    printed = capsys.readouterr().out
+    expected = (
+        "+-a-b-c-d-e-f-g-h-+\n"
+        "1                 |\n"
+        "2                 |\n"
+        "3       ·         |\n"
+        "4     · ● ○       |\n"
+        "5       ○ ● ·     |\n"
+        "6         ·       |\n"
+        "7                 |\n"
+        "8                 |\n"
+        "+-----------------+\n"
+    )
+
+    assert expected == printed
+
+
+def test_show_after_one_move(capsys: pytest.CaptureFixture[str]) -> None:
+    POSITION_AFTER_ONE_MOVE.show()
+
+    printed = capsys.readouterr().out
+    expected = (
+        "+-a-b-c-d-e-f-g-h-+\n"
+        "1                 |\n"
+        "2                 |\n"
+        "3     · ● ·       |\n"
+        "4       ● ●       |\n"
+        "5     · ● ○       |\n"
+        "6                 |\n"
+        "7                 |\n"
+        "8                 |\n"
+        "+-----------------+\n"
+    )
+
+    assert expected == printed
+
+
+@pytest.mark.parametrize(
+    ["index", "expected"],
+    [
+        pytest.param(0, "a1", id="index-0"),
+        pytest.param(7, "h1", id="index-7"),
+        pytest.param(56, "a8", id="index-56"),
+        pytest.param(63, "h8", id="index-63"),
+        pytest.param(17, "b3", id="index-17"),
+    ],
+)
+def test_index_to_field_ok(index: int, expected: str) -> None:
+    assert Position.index_to_field(index) == expected
+
+
+@pytest.mark.parametrize(
+    ["index"],
+    [
+        pytest.param(-1, id="too-small"),
+        pytest.param(64, id="too-big"),
+    ],
+)
+def test_index_to_field_error(index: int) -> None:
+    with pytest.raises(ValueError):
+        Position.index_to_field(index)
+
+
+@pytest.mark.parametrize(
+    ["indexes", "expected"],
+    [
+        pytest.param([], "", id="0-indexes"),
+        pytest.param([0], "a1", id="1-index"),
+        pytest.param([0, 1], "a1 b1", id="2-indexes"),
+    ],
+)
+def test_indexes_to_fields(indexes: Iterable[int], expected: str) -> None:
+    assert Position.indexes_to_fields(indexes) == expected
+
+
+@pytest.mark.parametrize(
+    ["field", "expected"],
+    [
+        pytest.param("a1", 0, id="field-a1"),
+        pytest.param("h1", 7, id="field-h1"),
+        pytest.param("a8", 56, id="field-a8"),
+        pytest.param("h8", 63, id="field-h8"),
+        pytest.param("b3", 17, id="field-b3"),
+        pytest.param("A1", 0, id="field-A1"),
+        pytest.param("H1", 7, id="field-H1"),
+        pytest.param("A8", 56, id="field-A8"),
+        pytest.param("H8", 63, id="field-H8"),
+        pytest.param("B3", 17, id="field-B3"),
+        pytest.param("--", PASS_MOVE, id="field---"),
+        pytest.param("ps", PASS_MOVE, id="field-ps"),
+        pytest.param("PS", PASS_MOVE, id="field-PS"),
+    ],
+)
+def test_field_to_index_ok(field: str, expected: int) -> None:
+    assert Position.field_to_index(field) == expected
+
+
+@pytest.mark.parametrize(
+    ["field"],
+    [
+        pytest.param("", id="empty"),
+        pytest.param("a", id="to-short"),
+        pytest.param("aaa", id="too-long"),
+        pytest.param("a9", id="invalid-row"),
+        pytest.param("i8", id="invalid-column"),
+    ],
+)
+def test_field_to_index_error(field: str) -> None:
+    with pytest.raises(ValueError):
+        assert Position.field_to_index(field)
+
+
+def test_rotate_move_normal() -> None:
+    move = 2
+    expected_rotated_moves = [2, 5, 58, 61, 16, 40, 23, 47]
+
+    for rotation in range(8):
+        expected = expected_rotated_moves[rotation]
+        assert Position.rotate_move(move, rotation) == expected
+
+
+def test_rotate_move_pass() -> None:
+    move = PASS_MOVE
+
+    for rotation in range(8):
+        assert Position.rotate_move(move, rotation) == PASS_MOVE
+
+
+@pytest.mark.parametrize(
+    ["move"],
+    [
+        pytest.param(2, id="normal"),
+        pytest.param(PASS_MOVE, id="pass"),
+    ],
+)
+def test_unrotate(move: int) -> None:
+    for rotation in range(8):
+        rotated_move = Position.rotate_move(move, rotation)
+        assert Position.unrotate_move(rotated_move, rotation) == move
+
+
+@pytest.mark.parametrize(
+    ["lhs", "rhs", "expected"],
+    [
+        pytest.param(POSITION_EMPTY, POSITION_EMPTY, True, id="equal"),
+        pytest.param(POSITION_EMPTY, POSITION_START, False, id="not-equal"),
+    ],
+)
+def test_eq_ok(lhs: Position, rhs: Position, expected: bool) -> None:
+    assert (lhs == rhs) == expected
+
+
+def test_eq_error() -> None:
+    with pytest.raises(TypeError):
+        POSITION_EMPTY == 0
+
+
+@pytest.mark.parametrize(
+    ["position", "expected"],
+    [
+        pytest.param(
+            POSITION_EMPTY,
+            "---------------------------------------------------------------- X;\n",
+            id="empty",
+        ),
+        pytest.param(
+            POSITION_START,
+            "---------------------------OX------XO--------------------------- X;\n",
+            id="start",
+        ),
+        pytest.param(
+            POSITION_AFTER_ONE_MOVE,
+            "-------------------O-------OO------OX--------------------------- X;\n",
+            id="after-one-move",
+        ),
+    ],
+)
+def test_to_problem(position: Position, expected: str) -> None:
+    assert position.to_problem() == expected
+
+
+@pytest.mark.parametrize(
+    ["position", "expected"],
+    [
+        pytest.param(POSITION_EMPTY, 0, id="empty"),
+        pytest.param(POSITION_START, 4, id="start"),
+        pytest.param(POSITION_AFTER_ONE_MOVE, 5, id="after-one-move"),
+    ],
+)
+def test_count_discs(position: Position, expected: int) -> None:
+    assert position.count_discs() == expected
+
+
+@pytest.mark.parametrize(
+    ["position", "expected"],
+    [
+        pytest.param(POSITION_EMPTY, 64, id="empty"),
+        pytest.param(POSITION_START, 60, id="start"),
+        pytest.param(POSITION_AFTER_ONE_MOVE, 59, id="after-one-move"),
+    ],
+)
+def test_count_empties(position: Position, expected: int) -> None:
+    assert position.count_empties() == expected
