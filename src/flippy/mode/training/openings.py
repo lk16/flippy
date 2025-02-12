7f355c19d85b46739a56a56741b2b459c67026f4
Luuk Verweij <luuk_verweij@msn.com>
2025-02-12 20:45:34 +0100

    support forced moves in exercises

---
+++ src/flippy/mode/training/openings.py
@@ -0,0 +1,7 @@
+from flippy.mode.training.exercise import Exercise
+from flippy.othello.board import BLACK, WHITE
+
+EXERCISES = [
+    Exercise(BLACK, "e6 f4 c3 c4 d3 d6 *f6 c6 c5"),
+    Exercise(WHITE, "e6 *f6 f5 d6 c5 e3"),
+]
