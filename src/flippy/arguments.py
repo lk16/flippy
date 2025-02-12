65db31d7f5f8c45b9fd998f82833b8001e0fcfa9
Luuk Verweij <luuk_verweij@msn.com>
2025-02-12 19:56:28 +0100

    simplify training mode a lot

--- src/flippy/arguments.py
+++ src/flippy/arguments.py
@@ -4,12 +4,6 @@ from pathlib import Path
 from typing import Optional
 
 
-class TrainingArguments:
-    def __init__(self, filters: list[str], top: Optional[int]) -> None:
-        self.filters = filters
-        self.top = top
-
-
 class PositionFrequencyArguments:
     def __init__(self, lost_only: bool, most_recent: Optional[int]) -> None:
         self.lost_only = lost_only
@@ -24,18 +18,15 @@ class PGNArguments:
 class Arguments:
     def __init__(
         self,
-        training: TrainingArguments,
         position_frequency: PositionFrequencyArguments,
         pgn: PGNArguments,
     ) -> None:
-        self.training = training
         self.position_frequency = position_frequency
         self.pgn = pgn
 
     @classmethod
     def empty(cls) -> Arguments:
         return Arguments(
-            TrainingArguments([], None),
             PositionFrequencyArguments(False, None),
             PGNArguments(None),
         )
