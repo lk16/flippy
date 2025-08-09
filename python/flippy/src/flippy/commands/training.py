fdf9ba4eba295e13b24389e5a5930a6775ca1cb2
Luuk Verweij <luuk_verweij@msn.com>
2025-08-09 11:38:34 +0200

    add show_board command

--- python/flippy/src/flippy/commands/training.py
+++ python/flippy/src/flippy/commands/training.py
@@ -2,7 +2,9 @@ import typer
 from pathlib import Path
 from typing import Annotated
 
+from flippy.othello.board import BLACK, WHITE, Board
 from flippy.othello.game import Game
+from flippy.othello.position import Position
 from flippy.training.file import DEFAULT_TRAINING_FILE_PATH, TrainingFile
 
 app = typer.Typer(pretty_exceptions_enable=False)
@@ -26,6 +28,25 @@ def show() -> None:
 def list_exercises() -> None:
     training_file = TrainingFile(DEFAULT_TRAINING_FILE_PATH)
     training_file.list_exercises()
+
+
+@app.command()
+def show_board(
+    board_string: Annotated[str, typer.Argument()],
+    white_turn: Annotated[bool, typer.Option("-w")] = False,
+) -> None:
+    if len(board_string) != 32:
+        raise typer.BadParameter("Board string must be 32 characters long")
+
+    if white_turn:
+        turn = WHITE
+    else:
+        turn = BLACK
+
+    me = int(board_string[:16], 16)
+    opp = int(board_string[16:], 16)
+
+    Board(Position(me, opp), turn).show()
 
 
 if __name__ == "__main__":
