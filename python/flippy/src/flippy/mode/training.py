06853481d74ef2c4cd409671a57610760979af18
Luuk Verweij <luuk_verweij@msn.com>
2025-08-09 12:21:05 +0200

    make sure grandchildren are consistent during training

--- python/flippy/src/flippy/mode/training.py
+++ python/flippy/src/flippy/mode/training.py
@@ -5,7 +5,7 @@ from flippy.arguments import Arguments
 from flippy.mode.game import GameMode
 from flippy.othello.board import Board
 from flippy.othello.position import InvalidMove
-from flippy.training.file import DEFAULT_TRAINING_FILE_PATH, Exercise, TrainingFile
+from flippy.training.file import DEFAULT_TRAINING_FILE_PATH, TrainingFile
 
 
 class TrainingMode(GameMode):
@@ -19,12 +19,21 @@ class TrainingMode(GameMode):
         self.exercises = exercises
         self.exercise_index = 0
         self.node_index = 0
+        self.board = self.get_new_exercise_board()
+
+    def get_new_exercise_board(self) -> Board:
+        exercise = self.exercises[self.exercise_index]
+
+        position = (
+            exercise.nodes[self.node_index]
+            .position.to_position()
+            .rotated(random.randint(0, 7))
+        )
+
+        return Board(position, exercise.color)
 
     def get_board(self) -> Board:
-        exercise: Exercise = self.exercises[self.exercise_index]
-        node = exercise.nodes[self.node_index]
-        position = node.position.to_position()
-        return Board(position, exercise.color)
+        return self.board
 
     def on_move(self, move: int) -> None:
         board = self.get_board()
@@ -32,6 +41,7 @@ class TrainingMode(GameMode):
         try:
             child = board.do_move(move)
         except InvalidMove:
+            # TODO show wrong moves played
             print("Invalid move")  # TODO remove
             return
 
@@ -44,6 +54,18 @@ class TrainingMode(GameMode):
 
         if self.node_index < len(exercise.nodes) - 1:
             self.node_index += 1
+
+            normalized_grand_child = (
+                self.exercises[self.exercise_index].nodes[self.node_index].position
+            )
+
+            valid_grand_children = []
+
+            for grand_child in child.get_children():
+                if grand_child.position.normalized() == normalized_grand_child:
+                    valid_grand_children.append(grand_child)
+
+            self.board = random.choice(valid_grand_children)
             return
 
         self.node_index = 0
